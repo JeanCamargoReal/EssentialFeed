84e6b70a901dd1fb497314c67934572bca4e06f1
Jean Camargo <jeancamargoreal@icloud.com>
2025-05-13 17:15:48 -0300

    Create FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jean Camargo on 13/05/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
